70e1ba669a4f92282b2bf4f449330eaab4772a9e
olivier Rukundo <rr.olivier01@gmail.com>
2025-07-09 20:07:11 +0200

    Added basic SELECT query using MYSQL

--- db.js
+++ db.js
@@ -19,3 +19,23 @@ conn.connect( err => {
         
     }
 })
+
+
+
+// step 2: pracical task with connection and running of SELECT query
+
+const query = 'SELECT * FROM users';
+
+conn.query(query, (err, results)=>{
+    if (err) {
+        console.log('Query error',err.message);
+        
+    }else{
+        console.log("User inserted successfully");
+        results.forEach(user => {
+            console.log(`${user.id}: ${user.name}: ${user.email}`);
+            
+            
+        })
+    }
+})
