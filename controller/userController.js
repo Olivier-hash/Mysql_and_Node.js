1b01beae5dd8e7afeac3fcaaea07e07591ef878f
olivier Rukundo <rr.olivier01@gmail.com>
2025-07-10 22:28:41 +0200

    Added Create users function by Sequlailze

--- controller/userController.js
+++ controller/userController.js
@@ -5,10 +5,11 @@ const userModel = require('../models/userModel');
 
 exports.CreateUSer = async (req, res) =>{
     const {fullName, email, password} =req.body
-}
 
-try {
-    
-} catch (error) {
-    
+  try {
+      const user = await userModel.create({fullName,email,password})
+      res.json({message:'user created sucessfully', user})
+  } catch (error) {
+      
+  }
 }
