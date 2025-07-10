50594ed4010d9f0cd6d3560881947f94e53caac2
olivier Rukundo <rr.olivier01@gmail.com>
2025-07-10 22:40:59 +0200

    Added validation of table users in database by Sequelize

--- models/userModel.js
+++ models/userModel.js
@@ -1,10 +1,24 @@
 
-const {DataTypes} = require('sequelize');
+const {DataTypes, STRING} = require('sequelize');
 const dbconn = require('../db')
 
 
 const Users = dbconn.define('users',{
     fullname:{
-        
+        type:DataTypes.STRING,
+        allowNull:false,
+        validate:{len:[5]}
+    },
+    email:{
+        type:DataTypes.STRING,
+        allowNull:false,
+        unique:true,
+        validate:{
+            isEmail:true
+        }
+    },password:{
+        type:DataTypes.STRING,
+        allowNull:false,
+        validate:{len:[8]}
     }
 })
