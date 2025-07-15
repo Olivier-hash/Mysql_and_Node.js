045410880c9685d2f5b5e0e556d7d3e5a35d3840
olivier Rukundo <rr.olivier01@gmail.com>
2025-07-15 19:35:01 +0200

    Added dotenv import

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
-const dotenv = process.dotenv.config()
-
+const dotenv = require('dotenv')
+dotenv.config();
 
 app.use('/users', (req,res)=>{
 
 });
 
+const PORT = process.env.PORT
 
 app.listen(PORT, ()=>{
     console.log(`🚀 Server running at http://localhost:${PORT}`);
