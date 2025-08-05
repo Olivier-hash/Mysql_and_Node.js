cd5758244482981ae0f7af9fa594787638f251b3
olivier Rukundo <rr.olivier01@gmail.com>
2025-08-05 13:28:23 +0200

    Added Multer diskStorage

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,5 +1,14 @@
 // server.js codes
 const multer = require('multer');
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '/tmp/my-uploads')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, file.fieldname + '-' + uniqueSuffix)
+  }
+})
 const upload = multer({dest: 'upload/'});
 
 
